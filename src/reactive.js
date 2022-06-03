c1cd8acf7511d810fbff7ca87343962e86433ba4
jack <jackzhoumine@gmail.com>
2022-06-04 00:08:49 +0800

    ✅:处理特殊情况

--- src/reactive.js
+++ src/reactive.js
@@ -2,17 +2,20 @@
  * @Description :
  * @Date        : 2022-06-03 23:02:27 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-06-03 23:57:41 +0800
+ * @LastEditTime: 2022-06-04 00:08:04 +0800
  * @LastEditors : JackChou
  */
-import { isOject } from './utils.js'
+import { isOject, isReactive } from './utils.js'
 import { track, trigger } from './effect.js'
 
 export function reactive(target) {
   if (!isOject(target)) return target
+  //NOTE 处理 reactive(reactive)
+  if (isReactive(target)) return target
   const proxy = new Proxy(target, {
     get(target, key, receiver) {
       console.log('get', key)
+      if (key === '__isReactive') return true
       const value = Reflect.get(target, key, receiver)
       // TODO 如何让副作用和proxy 发生联系
       // NOTE 收集副作用的依赖
--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
  * @Description : 工具函数
  * @Date        : 2022-06-03 22:53:29 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-06-03 23:07:47 +0800
+ * @LastEditTime: 2022-06-04 00:06:18 +0800
  * @LastEditors : JackChou
  */
 export function isOject(value) {
@@ -11,4 +11,8 @@ export function isOject(value) {
 
 export function isFn(value) {
   return typeof value === 'function'
+}
+
+export function isReactive(target) {
+  return target && target.__isReactive
 }
