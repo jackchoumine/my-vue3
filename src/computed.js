026ffd6b124c8f579660d65fcf74b69a927aa6cf
jack <jackzhoumine@gmail.com>
2022-06-04 05:07:48 +0800

    ✅:完成computed setter

--- src/computed.js
+++ src/computed.js
@@ -2,11 +2,12 @@
  * @Description : 计算属性
  * @Date        : 2022-06-04 03:37:25 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-06-04 04:58:49 +0800
+ * @LastEditTime: 2022-06-04 05:06:31 +0800
  * @LastEditors : JackChou
  */
-import { effect, track } from './effect.js'
+import { effect, track, trigger } from './effect.js'
 import { isObject } from './utils.js'
+
 export function computed(getterOrSetter) {
   let setter = () => {
       console.warn('computed is read only, please provide setter')
@@ -30,23 +31,25 @@ class Compute {
       lazy: true,
       //NOTE 依赖边触发，触发调度程序
       scheduler: () => {
-        console.log('computed scheduler')
-        this._dirty = true
+        if (!this._dirty) {
+          console.log('computed scheduler')
+          this._dirty = true
+          trigger(this, 'value')
+        }
       },
     })
   }
   get value() {
     if (this._dirty) {
-      // debugger
       this._value = this.effect()
       this._dirty = false
-      // debugger
       track(this, 'value')
     }
     return this._value
   }
   set value(value) {
     console.log('setter', value)
+    // trigger(this, 'value')
     this._setter(value)
   }
 }
